1bdc111574c621f2bf10343d4997c857636a53a5
Yorick Peterse <git@yorickpeterse.com>
2024-05-02 21:38:12 +0200

    Add support for setting the outside temperature
    This is updated every hour according to the Buienradar JSON API.

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ Daalderop heatpump using the [Itho WiFi
 module](https://github.com/arjenhiemstra/ithowifi), and sending it to an
 InfluxDB/VictoriaMetrics database.
 
+The script also supports controlling the outside temperature of the WPU, using
+[Buienradar](https://www.buienradar.nl/) as the temperature source. For this to
+work, you need to set `WEATHER_STATION_ID` to a Buienradar `stationid` value as
+found in [this JSON response](https://data.buienradar.nl/2.0/feed/json).
+
 # License
 
 All source code in this repository is licensed under the Mozilla Public License
--- monitor.fish
+++ monitor.fish
@@ -1,30 +1,33 @@
 #!/usr/bin/env fish
 
-if ! test -n "$ITHO_IP"
-    echo 'The ITHO_IP variable must be set'
-    exit 1
+function info
+    echo "$(date --iso-8601=seconds): $argv"
 end
 
-if ! test -n "$DB_IP"
-    echo 'The DB_IP variable must be set'
-    exit 1
+function http_get
+    curl --fail \
+        --show-error \
+        --silent \
+        --connect-timeout 30 \
+        --max-time 30 \
+        $argv
 end
 
-if ! test -n "$DB_PORT"
-    echo 'The DB_PORT variable must be set'
-    exit 1
-end
-
-while true
-    set data (
-        curl --fail \
-            --show-error \
-            --silent \
-            --connect-timeout 30 \
-            --max-time 30 \
-            "http://$ITHO_IP/api.html?get=ithostatus"
+function update_temp
+    info 'Updating outside temperature'
+    set temp (
+        http_get 'https://data.buienradar.nl/2.0/feed/json' \
+            | jq ".actual.stationmeasurements[] | select(.stationid == $WEATHER_STATION_ID) | .temperature | ceil"
     )
 
+    http_get "http://$ITHO_IP/api.html?outside_temp=$temp" >/dev/null
+    info "New outside temperature: $temp"C
+end
+
+function update_stats
+    set data (http_get "http://$ITHO_IP/api.html?get=ithostatus")
+
+    set outside_temp (echo $data | jq '."Outside temp (°C)"')
     set boiler_temp_down (echo $data | jq '."Boiler temp down (°C)"')
     set boiler_temp_up (echo $data | jq '."Boiler temp up (°C)"')
     set liquid_temp (echo $data | jq '."Liquid temp (°C)"')
@@ -47,6 +50,39 @@ wpu_liquid temp=$liquid_temp,flow_rate=$flow_hour
 wpu_source supply_temp=$from_src_temp,return_temp=$to_src_temp,pump_speed=$well_pump_speed
 wpu_cv supply_temp=$cv_supply_temp,return_temp=$cv_return_temp,pump_speed=$cv_pump_speed,pressure=$cv_pressure
 wpu_room temp=$room_temp,target_temp=$req_room_temp
+wpu_outside temp=$outside_temp
 wpu status=$system_status" | ncat --udp $DB_IP $DB_PORT
-    sleep 30
+end
+
+if ! test -n "$ITHO_IP"
+    echo 'The ITHO_IP variable must be set'
+    exit 1
+end
+
+if ! test -n "$DB_IP"
+    echo 'The DB_IP variable must be set'
+    exit 1
+end
+
+if ! test -n "$DB_PORT"
+    echo 'The DB_PORT variable must be set'
+    exit 1
+end
+
+set temp_interval 3600
+set temp_pending 0
+set sleep_interval 60
+
+while true
+    if test -n "$WEATHER_STATION_ID"
+        if test $temp_pending -eq 0
+            set temp_pending (math "$temp_interval / $sleep_interval")
+            update_temp
+        else
+            set temp_pending (math "$temp_pending - 1")
+        end
+    end
+
+    update_stats
+    sleep $sleep_interval
 end
